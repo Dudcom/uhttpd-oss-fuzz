4af927e31c54e23e7259cfc434632fafad7c545e
Dudcom <70006792+Dudcom@users.noreply.github.com>
2025-07-01 06:34:22 -0400

    init logic

--- uhttpd-fuzz.c
+++ uhttpd-fuzz.c
@@ -9,20 +9,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-typedef int socklen_t;
-#define AF_INET 2
-#define AF_INET6 10
-struct sockaddr_in {
-    short sin_family;
-    unsigned short sin_port;
-    struct in_addr sin_addr;
-    char sin_zero[8];
-};
-struct in_addr {
-    unsigned long s_addr;
-};
-#endif
-
 #include <libubox/blobmsg.h>
 #include "uhttpd.h"
 
